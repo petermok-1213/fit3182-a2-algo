54b84ea36bcf3932eccd3fff9fb712012fafb3c7
Peter Mok <43172966+petermok-1213@users.noreply.github.com>
2023-04-28 16:10:02 +1000

    ADDED comments, MODIFIED partition and pqsa

--- pqsa.py
+++ pqsa.py
@@ -1,38 +1,54 @@
 import random
 
+
+'''
+    A function for generating a list of random numbers
+
+    Input:
+        size: int
+        max: int
+    Output:
+        a list with length == size of random integers with values in the range of 0 to max-1 inclusively
+'''
 def gen_rand_data(size: int, max: int) -> list:
     int_list = []
     for _ in range(size):
         int_list.append(random.randrange(start=0, stop=max))
     return int_list
 
-def partition(arr: list[int], pivot_index: int) -> tuple[list[int], int]:
-    store_index = 0
-    for i in range(len(arr)):
-        if arr[i] <= arr[pivot_index] and i != pivot_index:
-            arr[store_index], arr[i] = arr[i], arr[store_index]
-            if pivot_index == store_index:
-                pivot_index = i
+
+'''
+    In-place Partitioning function for quick sort,
+    returns the pivot index
+
+    Input:
+        arr: list[int]
+    Output:
+        pivot_index: int
+'''
+def partition(arr: list[int], left, right) -> int:
+    pivot_index = random.randrange(start=left, stop=right)  # selecting a random index as pivot
+    store_index = left
+    for i in range(left+1, right):
+        if arr[i] <= arr[pivot_index] and i != pivot_index:                     # if element i is smaller than pivot
+            arr[store_index], arr[i] = arr[i], arr[store_index]                 # swap elements in i and store_index
+            if pivot_index == store_index:                                          # if element i is swapped with pivot     
+                pivot_index = i                                                     # pivot_index become i
             store_index += 1
 
-    arr[pivot_index], arr[store_index] = arr[store_index], arr[pivot_index]
-    pivot_index = store_index
-    return (arr, pivot_index)
+    arr[pivot_index], arr[store_index] = arr[store_index], arr[pivot_index]     # Swap elements of (pivot_index, store_index)
+    pivot_index = store_index                                                   # update pivot_index 
+    return pivot_index
 
-def pqsa(arr: list[int]) -> list[int]:
-    if len(arr) > 1:
-        pivot = random.randrange(start=0, stop=len(arr)-1)  # selecting a random index as pivot
-        x = pqsa(arr[:pivot])
-        y = pqsa(arr[pivot+1:])
-        print(x)
-        print(y)
-    else:
-        return arr
+def pqsa(arr: list[int], left, right) -> list[int]:
+    if right > left:
+        pivot = partition(arr, left, right)
+        pqsa(arr, left, pivot)
+        pqsa(arr, pivot+1, right)
 
 if __name__ == "__main__":
-    random.seed(123123123123)
-    data = gen_rand_data(10, 10)
+    random.seed(31266797)
+    data = gen_rand_data(15, 10)
     print(data)
-    print(partition(data, 4))
-    
-
+    pqsa(data, 0, len(data))
+    print(data)
