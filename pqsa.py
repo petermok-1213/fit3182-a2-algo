e72b7c7f50f8bef773550f6ed6718e2a35f2c1bc
Peter Mok <43172966+petermok-1213@users.noreply.github.com>
2023-05-05 14:46:37 +1000

    CHANGED PQSA from multiprocessing into multithreading

--- pqsa.py
+++ pqsa.py
@@ -1,23 +1,40 @@
-from multiprocessing import Pool
-from rand_qs import rand_qs, get_rand_data
+import copy
+import random
+import threading
+from rand_qs import get_rand_data, rand_partition, rand_qs
 import time
 
-def f():
-    n = 100
-    data = get_rand_data(n,10)
-    with Pool(processes=8) as pool:
-        start = time.time()
-        res = pool.apply_async(rand_qs, (data, 0, n-1))
-        end = time.time()
-        print("Parallel:", end-start)
+class PQSAThread(threading.Thread):
 
-    start = time.time()
-    rand_qs(get_rand_data(n, 10), 0, n-1)
-    end = time.time()
-    print("Serial:", end-start)
+    def __init__(self, arr: list[int], left: int, right: int):
+        threading.Thread.__init__(self)
+        self.arr = arr
+        self.left = left
+        self.right = right
 
-    print("jobs done")
+    def run(self):
+        print("Starting Thread ", threading.get_ident(), "\n")
+        if self.right > self.left:
+            pivot = rand_partition(self.arr, self.left, self.right)
+            l_child_thread = PQSAThread(self.arr, self.left, pivot)
+            r_child_thread = PQSAThread(self.arr, pivot+1, self.right)
+            l_child_thread.start()
+            r_child_thread.start()
+            if not l_child_thread.is_alive() and not r_child_thread.is_alive():
+                print("Ending Thread", threading.get_ident(), "\n")
+
+def pqsa(arr, left, right):
+    if right > left:
+        pivot = rand_partition(arr, left, right)
+        pqsa(arr, left, pivot)
+        pqsa(arr, pivot+1, right)
+    return arr
 
 
 if __name__ == '__main__':
 
+    random.seed(31266797)
+    n = 50
+    data = get_rand_data(n, 10)
+    main_pqsa_thread = PQSAThread(data, 0, n-1)
+    main_pqsa_thread.start()
