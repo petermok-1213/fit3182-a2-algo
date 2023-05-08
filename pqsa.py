be9314afc3074e3a63ff1ef59d9ac35c6d1d7e3c
Peter Mok <43172966+petermok-1213@users.noreply.github.com>
2023-05-08 16:10:52 +1000

    VERIFIED PQSA correctness

--- pqsa.py
+++ pqsa.py
@@ -1,5 +1,6 @@
 import copy
 import random
+import sys
 import threading
 from rand_qs import get_rand_data, rand_partition, rand_qs
 import time
@@ -13,28 +14,37 @@ class PQSAThread(threading.Thread):
         self.right = right
 
     def run(self):
-        print("Starting Thread ", threading.get_ident(), "\n")
         if self.right > self.left:
             pivot = rand_partition(self.arr, self.left, self.right)
             l_child_thread = PQSAThread(self.arr, self.left, pivot)
             r_child_thread = PQSAThread(self.arr, pivot+1, self.right)
-            l_child_thread.start()
-            r_child_thread.start()
-            if not l_child_thread.is_alive() and not r_child_thread.is_alive():
-                print("Ending Thread", threading.get_ident(), "\n")
-
-def pqsa(arr, left, right):
-    if right > left:
-        pivot = rand_partition(arr, left, right)
-        pqsa(arr, left, pivot)
-        pqsa(arr, pivot+1, right)
-    return arr
+            try:
+                l_child_thread.start()
+                r_child_thread.start()
+                l_child_thread.join()
+                r_child_thread.join()
+            except RuntimeError as error:
+                time.sleep(0.1)
 
 
 if __name__ == '__main__':
 
-    random.seed(31266797)
-    n = 50
-    data = get_rand_data(n, 10)
+    random.seed(3126679)
+    n = 1000000
+    sys.setrecursionlimit(n)    # force increase maximum recursion depth to avoid Exception
+
+    data = get_rand_data(n, 100)
+    test_data = copy.deepcopy(data)
+
     main_pqsa_thread = PQSAThread(data, 0, n-1)
+    start = time.time()
     main_pqsa_thread.start()
+    main_pqsa_thread.join()
+    pqsa_time = time.time()-start
+    print("Parallel: ", pqsa_time)
+
+    rand_qs_thread = threading.Thread(target=rand_qs, args=(test_data, 0, n-1))
+    start = time.time()
+    rand_qs_thread.start()
+    rand_qs_thread.join()
+    print("Serial: ", time.time()-start)
