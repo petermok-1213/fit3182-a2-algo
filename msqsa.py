e1c5147a90ea0b08034c75a2c9d71271628029a1
Peter Mok <43172966+petermok-1213@users.noreply.github.com>
2023-05-09 19:04:32 +1000

    COMPLETED MSQSA

--- msqsa.py
+++ msqsa.py
@@ -1,5 +1,46 @@
-from rand_qs import get_rand_data, rand_partition
+import random
+import threading
+from rand_qs import get_rand_data, it_rand_qs
 
+
+"""
+    Partition data based on the given number of threads
+    Input:
+        arr: list to be partitioned
+        n: number of threads
+    Output:
+        partitions: list of partitions each for one thread
+"""
+def partition_by_threads(arr: list[int], n: int, partitions: list[list[int]]) -> list[list[int]]:
+    cur_partition = []
+    cur_index = 0
+    while len(partitions) < n:
+        while (len(cur_partition) < len(arr)/n) and cur_index < len(arr):
+            cur_partition.append(arr[cur_index])
+            cur_index += 1
+        partitions.append(cur_partition)
+        cur_partition = []
+    return partitions
+
+"""
+    Iterative 2-way merge sort
+    Input:
+        partitions: list of partitions to be merged into 1
+    Output:
+        partitions[0]: the final merged partition
+"""
+def merge(partitions: list[list[int]]) -> list[int]:
+    while len(partitions) > 1:                      # while there are multiple partitions
+        left = partitions.pop(0)                    # pop top 2 partitions
+        right = partitions.pop(0)                   # --
+        result = []                                 # init placeholder for the sub-result
+        while len(left) > 0 and len(right) > 0:     # while they are not empty
+            if left[0] < right[0]:                  ###
+                result.append(left.pop(0))          # pop the smaller element to the end
+            else:                                   # of result
+                result.append(right.pop(0))         ###
+        partitions.append(result)                   # put result at the end of the queue
+    return partitions[0]                            # [[result]] -> [result]
 
 """
     Implementation of Merging Subarrys from Quick Sort Algorithm
@@ -8,8 +49,32 @@ from rand_qs import get_rand_data, rand_partition
         n: number of threads
 """
 def msqsa(arr: list[int], n: int) -> None:
-    pass
 
+    partitions = []
+    partition_thread = threading.Thread(
+        target=partition_by_threads,
+        args=(arr, n, partitions)
+    )
+    partition_thread.start()
+    partition_thread.join()
+
+    threads = []
+    for i in range(n):
+        threads.append(threading.Thread(
+            target=it_rand_qs,
+            args=(partitions[i], 0, len(partitions[i])-1)
+        ))
+    for thread in threads:
+        thread.start()
+    for thread in threads:
+        thread.join()
+
+    partitions = merge(partitions)
+    print(partitions)
 
 if __name__ == "__main__":
-    pass
+
+    random.seed(31266797)
+
+    msqsa(get_rand_data(1000, 10), 8)
+
