1d6de2523085a30452ccbe2ea37c7a5668692f63
Peter Mok <43172966+petermok-1213@users.noreply.github.com>
2023-05-22 20:52:35 +1000

    REMOVED not used modules

--- pqsa.py
+++ pqsa.py
@@ -1,11 +1,8 @@
-import random
-import threading
 from threadpool import ThreadPool
 from threading import Thread
 
-import rand_qs
 from rand_qs import rand_partition
-import time
+
 
 '''
     Class representing the master PQSA thread
@@ -18,7 +15,6 @@ def pqsa(arr: list[int], left: int, right: int):
         # Create child thread for sorting left/right partition
         ThreadPool().pool.put(Thread(target=pqsa, args=(arr, left, pivot)))     # Put threads in threadpool
         ThreadPool().pool.put(Thread(target=pqsa, args=(arr, pivot+1, right)))  # (block until there is enough space)
-
 
 
 if __name__ == '__main__':
